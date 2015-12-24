aecfb1475136b25a92aec6192f0cd97359afd643
Bart van Dorp <bartvandorp@raadhuis.com>
2015-12-24 15:11:17 +0100

    Gitify installer + path set via shell script

--- puphpet/files/exec-once/Gitify-install.sh
+++ puphpet/files/exec-once/Gitify-install.sh
@@ -2,4 +2,5 @@ git clone https://github.com/modmore/Gitify.git Gitify
 cd Gitify
 composer install
 sudo su
-echo "PATH=${PATH}:/home/vagrant/Gitify" > /etc/profile.d/Gitify.sh && chmod 755 /etc/profile.d/Gitify.s
+PATH=${PATH}:/home/vagrant/Gitify > /etc/profile.d/Gitify.sh && chmod 755 /etc/profile.d/Gitify.sh
+exit
