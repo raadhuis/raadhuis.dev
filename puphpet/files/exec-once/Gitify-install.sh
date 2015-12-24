8183461f8c089e726aea61ecbc829142dcba42ab
Bart van Dorp <bartvandorp@raadhuis.com>
2015-12-24 15:41:53 +0100

    Gitify juiste path

--- puphpet/files/exec-once/Gitify-install.sh
+++ puphpet/files/exec-once/Gitify-install.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/modmore/Gitify.git Gitify
 cd Gitify
 composer install
-sudo su
-PATH=${PATH}:/home/vagrant/Gitify > /etc/profile.d/Gitify.sh && chmod 755 /etc/profile.d/Gitify.sh
+echo 'export PATH=$PATH:~/Gitify/'  >> ~/.bash_profile
 exit
