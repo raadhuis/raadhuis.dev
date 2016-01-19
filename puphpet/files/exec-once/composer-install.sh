cadb9c97891b28765feba879a76d614e84105c90
Bart van Dorp <bartvandorp@raadhuis.com>
2016-01-19 18:56:30 +0100

    aanpassing maar nog steeds niet werkzaam

--- puphpet/files/exec-once/composer-install.sh
+++ puphpet/files/exec-once/composer-install.sh
@@ -1,2 +1,2 @@
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
-echo "PATH=${PATH}:/usr/bin" > /etc/profile.d/composer.sh && chmod 755 /etc/profile.d/composer.sh
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+echo "PATH=${PATH}:/usr/local/bin" > /etc/profile.d/composer.sh && chmod 755 /etc/profile.d/composer.sh
