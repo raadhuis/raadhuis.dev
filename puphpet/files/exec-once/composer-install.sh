07c90ce64284af1eebafcdfd7ff5dc285048e9bb
Bart van Dorp <bartvandorp@raadhuis.com>
2016-01-19 13:34:11 +0100

    composer install script aangepast

--- puphpet/files/exec-once/composer-install.sh
+++ puphpet/files/exec-once/composer-install.sh
@@ -1,4 +1,2 @@
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
-sudo su
-PATH=${PATH}:/usr/bin > /etc/profile.d/composer.sh && chmod 755 /etc/profile.d/composer.sh
-exit
+echo "PATH=${PATH}:/usr/bin" > /etc/profile.d/composer.sh && chmod 755 /etc/profile.d/composer.sh
